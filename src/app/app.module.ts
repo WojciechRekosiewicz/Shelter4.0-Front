11504711ae289a703132d24f0097a610998056cf
Gonska93 <gonska9933@gmail.com>
2019-06-17 21:00:23 +0500

    Add fontawesome cdn import and modify index html styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
+import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router'
 import { HttpClientModule } from '@angular/common/http';
-
-import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { NotFoundComponent } from './error-pages/not-found/not-found.component';
