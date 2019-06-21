c97cf80ff989e65f59331a35b7c1b67292272eeb
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-06-21 21:04:14 +0200

    cleaned home.component from unused code and change hello text

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { RepositoryService } from '../shared/services/repository.service';
-import { ErrorHandlerService } from '../shared/services/error-handler.service';
-import { Router, ActivatedRoute } from '@angular/router';
-import { AdvertShort } from '../_interfaces/advert-short.model';
 
 @Component({
   selector: 'app-home',
@@ -12,31 +8,12 @@ import { AdvertShort } from '../_interfaces/advert-short.model';
 export class HomeComponent implements OnInit {
 
   public homeText: string;
-  adverts: AdvertShort[] = [];
   errorMessage: any;
-  //constructor() { }
-
-  //ngOnInit() {
-  //  this.homeText = "WELCOME TO ACCOUNT-OWNER APPLICATION";
-  //}
-
-  constructor(private repository: RepositoryService,
-    private errorHandler: ErrorHandlerService,
-    private router: Router,
-    private route: ActivatedRoute) { }
+  constructor() { }
 
   ngOnInit() {
-
-    this.homeText = "WELCOME TO ACCOUNT-OWNER APPLICATION";
- 
-    let apiAddress: string = "api/adverts";
-    this.repository.getData(apiAddress).subscribe(
-      (adverts: any) => {
-        this.adverts = adverts;
-
-      },
-      error => this.errorMessage = <any>error
-    );
-
+    this.homeText = "WELCOME TO E-SHELTER";
   }
+
+
 }
